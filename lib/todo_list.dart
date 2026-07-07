d905db6b521822639391b30d72dd3e909e526a74
BIJOY-PAUL <bijoyrudrapaul78@gmail.com>
2026-07-07 09:45:37 +0600

    add floationgactionbutton

--- lib/todo_list.dart
+++ lib/todo_list.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:river_pod/item_provider.dart';
 
-class TodoList extends StatelessWidget {
+class TodoList extends ConsumerWidget {
   const TodoList({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("ToDo app")));
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(title: Text("ToDo app")),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          ref.read(itemProvider.notifier).addItem("Bijoy rudra paul");
+        },
+
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
